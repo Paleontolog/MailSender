2657cfae6b02f9ea46ee2d7a4cd46ec3a2c716f7
Paleontolog <lezgya@yandex.ru>
2019-07-02 00:34:07 +0400

    Hello)

---
+++ src/main/java/com/mailsender/demo/DatabaseAccessor/DatabaseAccessor.java
@@ -0,0 +1,5 @@
+package com.mailsender.demo.DatabaseAccessor;
+
+public class DatabaseAccessor {
+
+}
--- src/main/java/com/mailsender/demo/model/Addressees.java
+++ src/main/java/com/mailsender/demo/model/Addressees.java
@@ -1,10 +1,29 @@
 package com.mailsender.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "addressees")
 public class Addressees {
+
+    @Id
+    @GeneratedValue
+    private final Long id;
+
+    @NotBlank
     private final String email;
 
-    public Addressees(String email) {
+    public Addressees(Long id, String email) {
+        this.id = id;
         this.email = email;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getEmail() {
