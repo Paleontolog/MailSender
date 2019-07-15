3587efae027ddcb461f902b357d9a5ff7b9a7735
Paleontolog <lezgya@yandex.ru>
2019-07-15 05:10:02 +0400

    no comments

--- src/main/java/com/mailsender/demo/Application.java
+++ src/main/java/com/mailsender/demo/Application.java
@@ -1,16 +1,14 @@
 package com.mailsender.demo;
 
+import com.mailsender.demo.OtherClasses.SendEmailScheduleConfigured;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableScheduling
+//@SpringBootApplication
+//@EnableScheduling
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
--- src/main/java/com/mailsender/demo/OtherClasses/SendEmailSchedule.java
+++ src/main/java/com/mailsender/demo/OtherClasses/SendEmailSchedule.java
@@ -1,36 +1,36 @@
-package com.mailsender.demo.OtherClasses;
-
-import com.mailsender.demo.database.IDatabaseAccessor;
-import com.mailsender.demo.model.Addressees;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.util.Date;
-
-@Component
-public class SendEmailSchedule {
-
-    private final JavaMailSender emailSender;
-    private final IDatabaseAccessor databaseAccessor;
-
-    public SendEmailSchedule(JavaMailSender emailSender, IDatabaseAccessor databaseAccessor) {
-        this.emailSender = emailSender;
-        this.databaseAccessor = databaseAccessor;
-    }
-
-    @Scheduled(cron="0 0 17 * * MON-FRI")
-   // @Scheduled(fixedDelay = 5000)
-    public void sendEmail() {
-        System.out.println(new Date());
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("Naglui.eretick@yandex.ru");
-        for (Addressees addressees : databaseAccessor.getAllAddresses()) {
-            message.setTo(addressees.getEmail());
-            message.setSubject("Привет от Бэрримора");
-            message.setText("Пора пить чай");
-            this.emailSender.send(message);
-        }
-    }
-}
+//package com.mailsender.demo.OtherClasses;
+//
+//import com.mailsender.demo.database.IDatabaseAccessor;
+//import com.mailsender.demo.model.Addressees;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Date;
+//
+//@Component
+//public class SendEmailSchedule {
+//
+//    private final JavaMailSender emailSender;
+//    private final IDatabaseAccessor databaseAccessor;
+//
+//    public SendEmailSchedule(JavaMailSender emailSender, IDatabaseAccessor databaseAccessor) {
+//        this.emailSender = emailSender;
+//        this.databaseAccessor = databaseAccessor;
+//    }
+//
+//    @Scheduled(cron="0 0 17 * * MON-FRI")
+//   // @Scheduled(fixedDelay = 5000)
+//    public void sendEmail() {
+//        System.out.println(new Date());
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setFrom("Naglui.eretick@yandex.ru");
+//        for (Addressees addressees : databaseAccessor.getAllAddresses()) {
+//            message.setTo(addressees.getEmail());
+//            message.setSubject("Привет от Бэрримора");
+//            message.setText("Пора пить чай");
+//            this.emailSender.send(message);
+//        }
+//    }
+//}
